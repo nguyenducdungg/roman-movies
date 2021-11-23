bc3e0870f0ed9874f23a316c9c03a9ee13a137ed
Nguyễn Đức Dũng <nguyenducdung853@gmail.com>
2021-11-23 12:58:12 +0700

    update 23/11/2021

--- src/components/Rate/Rating/Rate.js
+++ src/components/Rate/Rating/Rate.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./Rate.css";
 import { FaStar } from "react-icons/fa";
 import axios from "../../axios";
@@ -6,34 +6,16 @@ import { NotificationContainer, NotificationManager } from 'react-notifications'
 const colors = {
     orange: "#FFBA5A",
     grey: "#a9a9a9"
-
 };
-
 //cách dùng : thêm Component Rate vào khi render các phim ra 
 // và truyền cho Component Rate id của Phim đó  
 // Sửa size thì thay đổi truyền size vào cho Rate
-
 const Rate = ({ id, size }) => {
     const user = JSON.parse(localStorage.getItem("user"));
-
 
     const [currentValue, setCurrentValue] = useState(0);
     const [hoverValue, setHoverValue] = useState(undefined);
     const stars = Array(5).fill(1)
-
-
-    useEffect(() => {
-        const rate = async () => {
-            const res = await axios.get(`/getratebymovie/${id}`)
-            let array = res.data.filter((el) => {
-                return el.user === user._id
-            })
-            if (array.length > 0) { setCurrentValue(array[0].rate) }
-        }
-        rate()
-    }, [id, user._id])
-
-
     const handleClick = async (value) => {
         setCurrentValue(value)
         if (!user) {
@@ -41,19 +23,14 @@ const Rate = ({ id, size }) => {
             setCurrentValue(0);
             return;
         }
-        try {
-            await axios.post("/createrate", { "rate": value, "user": `${user._id}`, "movie": `${id}` },
-                {
-                    headers: {
-                        "token": `${user.token}`
-                    }
-                })
-            NotificationManager.success('đánh giá của bạn', 'Cảm ơn về', 3000);
-        } catch (err) {
-            NotificationManager.warning(`${err.response.data.error}`,'', 3000)
-        } finally {
+        await axios.post("/createrate", { "rate": currentValue, "user": `${user._id}`, "movie": `${id}` },
+            {
+                headers: {
+                    "token": `${user.token}`
+                }
+            })
+        NotificationManager.success('đánh giá của bạn', 'Cảm ơn về', 3000);
 
-        }
     }
 
     const handleMouseOver = newHoverValue => {
